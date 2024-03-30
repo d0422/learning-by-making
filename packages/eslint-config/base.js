1f119b61d502a80e44afe3df59581d7ccfaf680c
d0422 <rlfehd2013@naver.com>
2024-03-30 10:38:23 +0900

    config: jsEventLoopVisualizer 프로젝트 세팅
    - eslint 설정 일부 변경

---
+++ packages/eslint-config/base.js
@@ -0,0 +1,37 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project,
+  },
+  extends: ['eslint:recommended', 'prettier', 'eslint-config-turbo'],
+  plugins: ['only-warn', '@typescript-eslint'],
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  env: {
+    browser: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    '.*.js',
+    'node_modules/',
+    'dist/',
+  ],
+  overrides: [
+    // Force ESLint to detect .tsx files
+    { files: ['*.js?(x)', '*.ts?(x)'] },
+  ],
+};
