39451473a49ca4afec2f996c324d1076db6dc938
d0422 <rlfehd2013@naver.com>
2024-04-08 12:13:52 +0900

    feat: 중첩적 함수 호출 처리를 위한 calleeName 추가

--- apps/jsEventLoopVisualizer/src/type.ts
+++ apps/jsEventLoopVisualizer/src/type.ts
@@ -2,6 +2,10 @@ import * as acorn from 'acorn';
 
 export interface TaskType {
   code: string;
-  expression: acorn.Expression | acorn.FunctionDeclaration;
+  expression:
+    | acorn.Expression
+    | acorn.FunctionDeclaration
+    | acorn.ExpressionStatement;
   executed: boolean;
+  calleeName?: string;
 }
