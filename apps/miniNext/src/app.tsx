b44e1e5a2f2036b1f9dbd1ad9957a81342749703
d0422 <rlfehd2013@naver.com>
2024-05-27 13:38:17 +0900

    feat: number API적용

--- apps/miniNext/src/app.tsx
+++ apps/miniNext/src/app.tsx
@@ -1,13 +1,17 @@
 import express from 'express';
 import App from './components/App';
 import { createHTML } from '@core/createHTML';
-
+let number = 0;
 const app = express();
 const port = 3000;
 app.use(express.static('dist/public'));
 app.get('/', (req, res) => {
   const html = createHTML(<App />);
   res.send(html);
+});
+
+app.get('/number', (req, res) => {
+  res.json({ number });
 });
 
 app.listen(port, () => {
