5a985499797828853bd85c16674aa6903facabb0
d0422 <rlfehd2013@naver.com>
2024-05-31 23:31:20 +0900

    feat: 재귀적으로 파일 이름을 가져오는 getPagesFiles 기능 추가

---
+++ apps/miniNext/src/core/getPagesFiles.ts
@@ -0,0 +1,29 @@
+import { Dirent, readdir } from 'fs';
+
+const PAGES_PATH = 'dist/pages';
+
+const fileProcess = async (pagesPath: string, file: Dirent) => {
+  if (file.isDirectory())
+    return await _getPagesFiles(`${pagesPath}/${file.name}`);
+
+  return `${pagesPath}/${file.name}`;
+};
+
+export const _getPagesFiles = async (pagesPath = PAGES_PATH) => {
+  const files: Dirent[] = await new Promise((resolve, reject) => {
+    readdir(pagesPath, { withFileTypes: true }, (err, files) => {
+      if (err) reject();
+      resolve(files);
+    });
+  });
+
+  const fileList = await Promise.all(
+    files.map(async (file) => await fileProcess(pagesPath, file))
+  );
+
+  return [...fileList.flat()];
+};
+
+export const getPagesFiles = async () => {
+  return (await _getPagesFiles()).map((file) => file.split(PAGES_PATH)[1]);
+};
