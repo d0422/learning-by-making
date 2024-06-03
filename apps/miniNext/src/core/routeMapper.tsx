758653049e8abf552ae36a9cde94b3a677345126
d0422 <rlfehd2013@naver.com>
2024-06-03 23:17:04 +0900

    feat: nested routing 지원

--- apps/miniNext/src/core/routeMapper.tsx
+++ apps/miniNext/src/core/routeMapper.tsx
@@ -18,17 +18,16 @@ export const findPagesFiles = async (pagesPath = PAGES_PATH) => {
 
 const getRoutingPath = (fileName: string) => {
   if (fileName === 'index') return '/';
+  if (fileName.match('index')) return `/${fileName.replace('index', '')}`;
   return `/${fileName.toLowerCase()}`;
 };
 
 export const routeMapper = async (app: Express) => {
   const files = await getPagesFiles();
-
   files.forEach((fullFileName) => {
     const fileName = fullFileName
       .replace(/dist\/pages\//, '')
       .replace(/.js/, '');
-
     app.get(
       getRoutingPath(fileName),
 
